421ae978624f1efd5734b38e55879f285b3e4f64
shadow-kun <shadowreaper@shadowreaper.net>
2018-04-30 14:44:10 +1000

    [Fix] Check

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,9 @@
 # Class: puppet_munki
 # ===========================
 #
-# Full description of class puppet_munki here.
+# A puppet module that allows multiple munki installations via an array input.
+# It is designed to be used with foreman and other modules. It also limits the
+# use to Darwin / OSX as munki is specialized to that OS.
 #
 # Parameters
 # ----------
@@ -42,7 +44,81 @@
 #
 # Copyright 2018 Your name here, unless otherwise noted.
 #
-class puppet_munki {
+class puppet_munki (
+    $munki_repo_url = 'http://munki/munki_repo/',
+    $munki_clientidentifier = '',
+    $munki_additionalheaders = '') {
+
+      # Check for OSX and run only if OSX
+      if $facts['os']['family'] == 'Darwin' {
+          class { 'mac_admin::munki':
+            repourl          => $munki_repo_url,
+            suppress_stop    => true,
+            bootstrap        => true,
+            clientidentifier => $munki_clientidentifier,
+        }
+      }
+    }
 
 
-}
+#        # checks to see if an internal server source is set.
+#        if(!$chocolatey_internal_server.empty)
+#        {
+#          # Sets if required internal sources only.
+#          if($chocolatey_internal_only == true)
+#          {
+#            $chocolateysource = disabled
+#          }
+#          else
+#          {
+#            $chocolateysource = present
+#          }
+#
+#          # Sets internal server settings and makes it the primary source
+#          chocolateysource {'internal':
+#            ensure   => present,
+#            location => $chocolatey_internal_server,
+##          }
+  #        chocolateysource {'chocolatey':
+  #          ensure   => $chocolateysource,
+  #          location => 'https://chocolatey.org/api/v2',
+  #          priority => 0,
+  #        }
+  #      }
+#
+#        # Installs the application(s) listed if missing only
+#        if(!$package_install.empty)
+##          $package_install.each
+  #        |Integer $p_install_index, String $p_install_value| {
+  ##            ensure   => installed,
+    #          provider => chocolatey,
+    #        }
+    #      }
+    #    }
+#
+#
+#        # Installs the latest known version of application(s) listed
+#        if(!$package_latest.empty)
+#        {
+#          $package_latest.each
+#          |Integer $p_latest_index, String $p_latest_value| {
+#            package { $p_latest_value:
+#              ensure   => latest,
+#              provider => chocolatey,
+#            }
+#          }
+#        }
+
+#        # Uninstalls all application(s) listed
+#        if(!$package_absent.empty)
+#        {
+#          $package_absent.each
+#          |Integer $p_absent_index, String $p_absent_value| {
+#            package { $p_absent_value:
+#              ensure   => absent,
+#              provider => chocolatey,
+#            }
+#          }
+#        }
+#      }
+#    }
