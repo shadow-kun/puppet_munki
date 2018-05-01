60a748b9c2632f59c065fc958095be7899ca0ffb
shadow-kun <shadowreaper@shadowreaper.net>
2018-05-01 15:12:44 +1000

    [Mod] minor Fix

---
+++ Puppetfile
@@ -0,0 +1,5 @@
+forge 'https://forge.puppetlabs.com'
+
+mod 'grahamgilbert-mac_admin', '0.2.1'
+mod 'grahamgilbert-macdefaults'
+mod 'rcoleman-mac_profiles_handler'
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,27 @@
 require 'spec_helper'
+
+
+
 describe 'puppet_munki' do
+  let(:facts) do {
+    'os' => {
+      'name'   => 'Undef',
+      'family' => 'Undef'
+    }
+  }
+  end
+
   context 'with default values for all parameters' do
     it { should contain_class('puppet_munki') }
+    it { is_expected.to compile.with_all_deps }
+  end
+
+  context 'with default values set for windows' do
+
+      #let(:facts) do {
+      #  'os' => {
+      #    'name'   => 'windows',
+      #    'family' => 'windows'
+      #  },
   end
 end
